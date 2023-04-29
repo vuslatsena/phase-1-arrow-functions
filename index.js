2338e02d2111f4b73845c6c318b14ba36d0d535b
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 09:03:29 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = () =>{
+  return 2000/100;
+}
+
+const square = (parameter) =>{
+  return parameter*parameter;
+}
+
+const add = (parameter1, parameter2) =>{
+  return parameter1 + parameter2;
+}
